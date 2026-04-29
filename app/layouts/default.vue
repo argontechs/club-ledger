5073503d4562a95368cc6a18303721971afdafa5
brendany <79629480+TheInfexted@users.noreply.github.com>
2026-04-29 20:51:47 +0800

    feat(frontend): wire layout for mobile drawer + responsive padding

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="flex min-h-screen bg-[#F0F2F5]">
-    <Sidebar />
-    <div class="flex-1 flex flex-col min-w-0">
+    <Sidebar class="hidden lg:flex" />
+    <LayoutAppDrawer />
+    <div class="flex-1 min-w-0 flex flex-col">
       <Header>
         <template #actions>
           <slot name="header-actions" />
         </template>
       </Header>
-      <main class="flex-1 p-6">
+      <main class="flex-1 p-4 lg:p-6">
         <slot />
       </main>
     </div>
